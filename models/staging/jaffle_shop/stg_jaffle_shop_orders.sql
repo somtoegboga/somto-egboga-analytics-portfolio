83377331fcf82013a1098ec584a36d93fe846c7d
egbogasomto@gmail.com <egbogasomto@gmail.com>
2026-03-15 18:51:16 +0000

    added generic data tests (not null, unique, accepted values, relationships)

--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -2,7 +2,8 @@
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status as order_status,
+        _etl_loaded_at
 
     /*from raw.jaffle_shop.orders*/
     from {{ source('jaffle_shop', 'orders') }}
